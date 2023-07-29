51c324f25e5191df91d228aab0cdc55954d0c037
George <84743235+nikibeep@users.noreply.github.com>
2023-07-29 17:08:38 +0530

    Revert "first commit"
    This reverts commit d059cd65f18c09ed8d75d9bb4b03a431bfc4de8d.
    
     Changes to be committed:
    	deleted:    hello/.gitignore
    	deleted:    hello/Cargo.lock
    	deleted:    hello/Cargo.toml
    	deleted:    hello/src/main.rs
    	deleted:    hello/target/.rustc_info.json
    	deleted:    hello/target/CACHEDIR.TAG
    	deleted:    hello/target/debug/.cargo-lock
    	deleted:    hello/target/debug/.fingerprint/hello-58ee129c69a31725/bin-hello
    	deleted:    hello/target/debug/.fingerprint/hello-58ee129c69a31725/bin-hello.json
    	deleted:    hello/target/debug/.fingerprint/hello-58ee129c69a31725/dep-bin-hello
    	deleted:    hello/target/debug/.fingerprint/hello-58ee129c69a31725/invoked.timestamp
    	deleted:    hello/target/debug/.fingerprint/hello-808dc230f7b7e203/bin-hello
    	deleted:    hello/target/debug/.fingerprint/hello-808dc230f7b7e203/bin-hello.json
    	deleted:    hello/target/debug/.fingerprint/hello-808dc230f7b7e203/dep-bin-hello
    	deleted:    hello/target/debug/.fingerprint/hello-808dc230f7b7e203/invoked.timestamp
    	deleted:    hello/target/debug/.fingerprint/hello-b6e940d97040607d/dep-test-bin-hello
    	deleted:    hello/target/debug/.fingerprint/hello-b6e940d97040607d/invoked.timestamp
    	deleted:    hello/target/debug/.fingerprint/hello-b6e940d97040607d/test-bin-hello
    	deleted:    hello/target/debug/.fingerprint/hello-b6e940d97040607d/test-bin-hello.json
    	deleted:    hello/target/debug/deps/hello-58ee129c69a31725.d
    	deleted:    hello/target/debug/deps/hello-808dc230f7b7e203.d
    	deleted:    hello/target/debug/deps/hello-b6e940d97040607d.d
    	deleted:    hello/target/debug/deps/libhello-808dc230f7b7e203.rmeta
    	deleted:    hello/target/debug/deps/libhello-b6e940d97040607d.rmeta
    	deleted:    hello/target/debug/hello.d
    	deleted:    hello/target/debug/incremental/hello-1sqw264mwodk4/s-gna050zu7y-msyd40-f31nli1h1vfv8gtja87g1oq64/dep-graph.bin
    	deleted:    hello/target/debug/incremental/hello-1sqw264mwodk4/s-gna050zu7y-msyd40-f31nli1h1vfv8gtja87g1oq64/query-cache.bin
    	deleted:    hello/target/debug/incremental/hello-1sqw264mwodk4/s-gna050zu7y-msyd40-f31nli1h1vfv8gtja87g1oq64/work-products.bin
    	deleted:    hello/target/debug/incremental/hello-1sqw264mwodk4/s-gna050zu7y-msyd40.lock
    	deleted:    hello/target/debug/incremental/hello-3o1fhadtvlf2d/s-gna050zrcr-ylx9g5-62q1votcizw76v0wuldgi6epm/dep-graph.bin
    	deleted:    hello/target/debug/incremental/hello-3o1fhadtvlf2d/s-gna050zrcr-ylx9g5-62q1votcizw76v0wuldgi6epm/query-cache.bin
    	deleted:    hello/target/debug/incremental/hello-3o1fhadtvlf2d/s-gna050zrcr-ylx9g5-62q1votcizw76v0wuldgi6epm/work-products.bin
    	deleted:    hello/target/debug/incremental/hello-3o1fhadtvlf2d/s-gna050zrcr-ylx9g5.lock
    	deleted:    hello/target/debug/incremental/hello-3uwdssvsacncx/s-gna004uy1e-5ybms-1id4yfhvzfvjlucptsaiqh31b/2cbqkf0je4moldqp.o
    	deleted:    hello/target/debug/incremental/hello-3uwdssvsacncx/s-gna004uy1e-5ybms-1id4yfhvzfvjlucptsaiqh31b/2pmx1z7qwqv4pf2p.o
    	deleted:    hello/target/debug/incremental/hello-3uwdssvsacncx/s-gna004uy1e-5ybms-1id4yfhvzfvjlucptsaiqh31b/43s6l1krhsa2is3u.o
    	deleted:    hello/target/debug/incremental/hello-3uwdssvsacncx/s-gna004uy1e-5ybms-1id4yfhvzfvjlucptsaiqh31b/4aiapzmohoi9ss53.o
    	deleted:    hello/target/debug/incremental/hello-3uwdssvsacncx/s-gna004uy1e-5ybms-1id4yfhvzfvjlucptsaiqh31b/4yhe7jey8fq87yfm.o
    	deleted:    hello/target/debug/incremental/hello-3uwdssvsacncx/s-gna004uy1e-5ybms-1id4yfhvzfvjlucptsaiqh31b/9aeq6ejvyu502ph.o
    	deleted:    hello/target/debug/incremental/hello-3uwdssvsacncx/s-gna004uy1e-5ybms-1id4yfhvzfvjlucptsaiqh31b/dep-graph.bin
    	deleted:    hello/target/debug/incremental/hello-3uwdssvsacncx/s-gna004uy1e-5ybms-1id4yfhvzfvjlucptsaiqh31b/query-cache.bin
    	deleted:    hello/target/debug/incremental/hello-3uwdssvsacncx/s-gna004uy1e-5ybms-1id4yfhvzfvjlucptsaiqh31b/work-products.bin
    	deleted:    hello/target/debug/incremental/hello-3uwdssvsacncx/s-gna004uy1e-5ybms.lock
    
     Untracked files:
    	hello/

--- hello/target/debug/deps/hello-b6e940d97040607d.d
+++
@@ -1,5 +0,0 @@
-E:\Rust_Projects\hello\target\debug\deps\hello-b6e940d97040607d.rmeta: src\main.rs
-
-E:\Rust_Projects\hello\target\debug\deps\hello-b6e940d97040607d.d: src\main.rs
-
-src\main.rs:
--- hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-E:\Rust_Projects\hello\target\debug\hello.exe: E:\Rust_Projects\hello\src\main.rs
